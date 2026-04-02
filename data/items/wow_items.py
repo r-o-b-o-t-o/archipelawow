23f4cec881eca5408103c3d14853536770872319
Axel Cocat <ax.cocat@gmail.com>
2026-04-02 11:14:08 +0200

    feat(items/wow_items): add paladin glyphs

--- data/items/wow_items.py
+++ data/items/wow_items.py
@@ -17,6 +17,8 @@ class WoWItemsContainer(ItemContainer):
         match world.options.character_class:
             case CharacterClass.option_warrior:
                 items += GLYPHS_WARRIOR
+            case CharacterClass.option_paladin:
+                items += GLYPHS_PALADIN
 
         return items
 
@@ -113,4 +115,75 @@ GLYPHS_WARRIOR = [
     GLYPH_MINOR_WARRIOR_THUNDER_CLAP,
     GLYPH_MINOR_WARRIOR_CHARGE,
     GLYPH_MINOR_WARRIOR_BATTLE,
+]
+
+GLYPH_MAJOR_PALADIN_AVENGING_WRATH = WoWItem("Glyph of Avenging Wrath", 41107, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_AVENGERS_SHIELD = WoWItem("Glyph of Avenger's Shield", 41101, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SEAL_OF_VENGEANCE = WoWItem("Glyph of Seal of Vengeance", 43869, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SEAL_OF_RIGHTEOUSNESS = WoWItem("Glyph of Seal of Righteousness", 43868, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_HOLY_WRATH = WoWItem("Glyph of Holy Wrath", 43867, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_HAMMER_OF_WRATH = WoWItem("Glyph of Hammer of Wrath", 41097, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SALVATION = WoWItem("Glyph of Salvation", 45747, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_HOLY_SHOCK = WoWItem("Glyph of Holy Shock", 45746, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_DIVINE_PLEA = WoWItem("Glyph of Divine Plea", 45745, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SHIELD_OF_RIGHTEOUSNESS = WoWItem("Glyph of Shield of Righteousness", 45744, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_DIVINE_STORM = WoWItem("Glyph of Divine Storm", 45743, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_HAMMER_OF_THE_RIGHTEOUS = WoWItem("Glyph of Hammer of the Righteous", 45742, ItemClassification.useful)
+# GLYPH_MAJOR_PALADIN_BEACON_OF_LIGHT = WoWItem("Glyph of Beacon of Light", 45741, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SEAL_OF_WISDOM = WoWItem("Glyph of Seal of Wisdom", 41109, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SEAL_OF_LIGHT = WoWItem("Glyph of Seal of Light", 41110, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_CLEANSING = WoWItem("Glyph of Cleansing", 41104, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_TURN_EVIL = WoWItem("Glyph of Turn Evil", 41102, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_FLASH_OF_LIGHT = WoWItem("Glyph of Flash of Light", 41105, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_EXORCISM = WoWItem("Glyph of Exorcism", 41103, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_CONSECRATION = WoWItem("Glyph of Consecration", 41099, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_CRUSADER_STRIKE = WoWItem("Glyph of Crusader Strike", 41098, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_SEAL_OF_COMMAND = WoWItem("Glyph of Seal of Command", 41094, ItemClassification.useful)
+# GLYPH_MAJOR_PALADIN_RIGHTEOUS_DEFENSE = WoWItem("Glyph of Righteous Defense", 41100, ItemClassification.useful)
+# GLYPH_MAJOR_PALADIN_SPIRITUAL_ATTUNEMENT = WoWItem("Glyph of Spiritual Attunement", 41096, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_DIVINITY = WoWItem("Glyph of Divinity", 41108, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_HAMMER_OF_JUSTICE = WoWItem("Glyph of Hammer of Justice", 41095, ItemClassification.useful)
+GLYPH_MAJOR_PALADIN_JUDGEMENT = WoWItem("Glyph of Judgement", 41092, ItemClassification.useful)
+# GLYPH_MAJOR_PALADIN_HOLY_LIGHT = WoWItem("Glyph of Holy Light", 41106, ItemClassification.useful)
+GLYPH_MINOR_PALADIN_THE_WISE = WoWItem("Glyph of the Wise", 43369, ItemClassification.filler)
+GLYPH_MINOR_PALADIN_BLESSING_OF_KINGS = WoWItem("Glyph of Blessing of Kings", 43365, ItemClassification.filler)
+GLYPH_MINOR_PALADIN_SENSE_UNDEAD = WoWItem("Glyph of Sense Undead", 43368, ItemClassification.filler)
+GLYPH_MINOR_PALADIN_BLESSING_OF_WISDOM = WoWItem("Glyph of Blessing of Wisdom", 43366, ItemClassification.filler)
+GLYPH_MINOR_PALADIN_LAY_ON_HANDS = WoWItem("Glyph of Lay on Hands", 43367, ItemClassification.filler)
+GLYPH_MINOR_PALADIN_BLESSING_OF_MIGHT = WoWItem("Glyph of Blessing of Might", 43340, ItemClassification.filler)
+GLYPHS_PALADIN = [
+    GLYPH_MAJOR_PALADIN_AVENGING_WRATH,
+    GLYPH_MAJOR_PALADIN_AVENGERS_SHIELD,
+    GLYPH_MAJOR_PALADIN_SEAL_OF_VENGEANCE,
+    GLYPH_MAJOR_PALADIN_SEAL_OF_RIGHTEOUSNESS,
+    GLYPH_MAJOR_PALADIN_HOLY_WRATH,
+    GLYPH_MAJOR_PALADIN_HAMMER_OF_WRATH,
+    GLYPH_MAJOR_PALADIN_SALVATION,
+    GLYPH_MAJOR_PALADIN_HOLY_SHOCK,
+    GLYPH_MAJOR_PALADIN_DIVINE_PLEA,
+    GLYPH_MAJOR_PALADIN_SHIELD_OF_RIGHTEOUSNESS,
+    GLYPH_MAJOR_PALADIN_DIVINE_STORM,
+    GLYPH_MAJOR_PALADIN_HAMMER_OF_THE_RIGHTEOUS,
+    # GLYPH_MAJOR_PALADIN_BEACON_OF_LIGHT,
+    GLYPH_MAJOR_PALADIN_SEAL_OF_WISDOM,
+    GLYPH_MAJOR_PALADIN_SEAL_OF_LIGHT,
+    GLYPH_MAJOR_PALADIN_CLEANSING,
+    GLYPH_MAJOR_PALADIN_TURN_EVIL,
+    GLYPH_MAJOR_PALADIN_FLASH_OF_LIGHT,
+    GLYPH_MAJOR_PALADIN_EXORCISM,
+    GLYPH_MAJOR_PALADIN_CONSECRATION,
+    GLYPH_MAJOR_PALADIN_CRUSADER_STRIKE,
+    GLYPH_MAJOR_PALADIN_SEAL_OF_COMMAND,
+    # GLYPH_MAJOR_PALADIN_RIGHTEOUS_DEFENSE,
+    # GLYPH_MAJOR_PALADIN_SPIRITUAL_ATTUNEMENT,
+    GLYPH_MAJOR_PALADIN_DIVINITY,
+    GLYPH_MAJOR_PALADIN_HAMMER_OF_JUSTICE,
+    GLYPH_MAJOR_PALADIN_JUDGEMENT,
+    # GLYPH_MAJOR_PALADIN_HOLY_LIGHT,
+    GLYPH_MINOR_PALADIN_THE_WISE,
+    GLYPH_MINOR_PALADIN_BLESSING_OF_KINGS,
+    GLYPH_MINOR_PALADIN_SENSE_UNDEAD,
+    GLYPH_MINOR_PALADIN_BLESSING_OF_WISDOM,
+    GLYPH_MINOR_PALADIN_LAY_ON_HANDS,
+    GLYPH_MINOR_PALADIN_BLESSING_OF_MIGHT,
 ]
