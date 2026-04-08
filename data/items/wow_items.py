2aa7f2e8437dcffcce28bb82477d1fcbccb38fe9
Axel Cocat <ax.cocat@gmail.com>
2026-04-08 23:22:53 +0200

    feat(items/wow_items): add mage glyphs

--- data/items/wow_items.py
+++ data/items/wow_items.py
@@ -481,3 +481,78 @@ GLYPHS_DRUID = [
     GLYPH_MINOR_DRUID_THORNS,
     GLYPH_MINOR_DRUID_THE_WILD,
 ]
+
+GLYPH_MAJOR_MAGE_FROSTFIRE = WoWItem("Glyph of Frostfire", 44684, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_INVISIBILITY = WoWItem("Glyph of Invisibility", 42748, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ICE_LANCE = WoWItem("Glyph of Ice Lance", 42745, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_MOLTEN_ARMOR = WoWItem("Glyph of Molten Armor", 42751, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ARCANE_BLAST = WoWItem("Glyph of Arcane Blast", 44955, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ETERNAL_WATER = WoWItem("Glyph of Eternal Water", 50045, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_WATER_ELEMENTAL = WoWItem("Glyph of Water Elemental", 42754, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_LIVING_BOMB = WoWItem("Glyph of Living Bomb", 45737, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_DEEP_FREEZE = WoWItem("Glyph of Deep Freeze", 45736, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ARCANE_BARRAGE = WoWItem("Glyph of Arcane Barrage", 45738, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ICE_BARRIER = WoWItem("Glyph of Ice Barrier", 45740, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ARCANE_POWER = WoWItem("Glyph of Arcane Power", 42736, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_MIRROR_IMAGE = WoWItem("Glyph of Mirror Image", 45739, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_MAGE_ARMOR = WoWItem("Glyph of Mage Armor", 42749, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_MANA_GEM = WoWItem("Glyph of Mana Gem", 42750, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ICE_BLOCK = WoWItem("Glyph of Ice Block", 42744, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_SCORCH = WoWItem("Glyph of Scorch", 42747, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_EVOCATION = WoWItem("Glyph of Evocation", 42738, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_BLINK = WoWItem("Glyph of Blink", 42737, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ICY_VEINS = WoWItem("Glyph of Icy Veins", 42746, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_REMOVE_CURSE = WoWItem("Glyph of Remove Curse", 42753, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ARCANE_EXPLOSION = WoWItem("Glyph of Arcane Explosion", 42734, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_FROST_NOVA = WoWItem("Glyph of Frost Nova", 42741, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ICE_ARMOR = WoWItem("Glyph of Ice Armor", 42743, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_POLYMORPH = WoWItem("Glyph of Polymorph", 42752, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_ARCANE_MISSILES = WoWItem("Glyph of Arcane Missiles", 42735, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_FIRE_BLAST = WoWItem("Glyph of Fire Blast", 42740, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_FROSTBOLT = WoWItem("Glyph of Frostbolt", 42742, ItemClassification.useful)
+GLYPH_MAJOR_MAGE_FIREBALL = WoWItem("Glyph of Fireball", 42739, ItemClassification.useful)
+GLYPH_MINOR_MAGE_BLAST_WAVE = WoWItem("Glyph of Blast Wave", 44920, ItemClassification.filler)
+GLYPH_MINOR_MAGE_FROST_WARD = WoWItem("Glyph of Frost Ward", 43360, ItemClassification.filler)
+GLYPH_MINOR_MAGE_FIRE_WARD = WoWItem("Glyph of Fire Ward", 43357, ItemClassification.filler)
+GLYPH_MINOR_MAGE_SLOW_FALL = WoWItem("Glyph of Slow Fall", 43364, ItemClassification.filler)
+GLYPH_MINOR_MAGE_THE_PENGUIN = WoWItem("Glyph of the Penguin", 43361, ItemClassification.filler)
+GLYPH_MINOR_MAGE_ARCANE_INTELLECT = WoWItem("Glyph of Arcane Intellect", 43339, ItemClassification.filler)
+GLYPH_MINOR_MAGE_FROST_ARMOR = WoWItem("Glyph of Frost Armor", 43359, ItemClassification.filler)
+GLYPHS_MAGE = [
+    GLYPH_MAJOR_MAGE_FROSTFIRE,
+    GLYPH_MAJOR_MAGE_INVISIBILITY,
+    GLYPH_MAJOR_MAGE_ICE_LANCE,
+    GLYPH_MAJOR_MAGE_MOLTEN_ARMOR,
+    GLYPH_MAJOR_MAGE_ARCANE_BLAST,
+    GLYPH_MAJOR_MAGE_ETERNAL_WATER,
+    GLYPH_MAJOR_MAGE_WATER_ELEMENTAL,
+    GLYPH_MAJOR_MAGE_LIVING_BOMB,
+    GLYPH_MAJOR_MAGE_DEEP_FREEZE,
+    GLYPH_MAJOR_MAGE_ARCANE_BARRAGE,
+    GLYPH_MAJOR_MAGE_ICE_BARRIER,
+    GLYPH_MAJOR_MAGE_ARCANE_POWER,
+    GLYPH_MAJOR_MAGE_MIRROR_IMAGE,
+    GLYPH_MAJOR_MAGE_MAGE_ARMOR,
+    GLYPH_MAJOR_MAGE_MANA_GEM,
+    GLYPH_MAJOR_MAGE_ICE_BLOCK,
+    GLYPH_MAJOR_MAGE_SCORCH,
+    GLYPH_MAJOR_MAGE_EVOCATION,
+    GLYPH_MAJOR_MAGE_BLINK,
+    GLYPH_MAJOR_MAGE_ICY_VEINS,
+    GLYPH_MAJOR_MAGE_REMOVE_CURSE,
+    GLYPH_MAJOR_MAGE_ARCANE_EXPLOSION,
+    GLYPH_MAJOR_MAGE_FROST_NOVA,
+    GLYPH_MAJOR_MAGE_ICE_ARMOR,
+    GLYPH_MAJOR_MAGE_POLYMORPH,
+    GLYPH_MAJOR_MAGE_ARCANE_MISSILES,
+    GLYPH_MAJOR_MAGE_FIRE_BLAST,
+    GLYPH_MAJOR_MAGE_FROSTBOLT,
+    GLYPH_MAJOR_MAGE_FIREBALL,
+    GLYPH_MINOR_MAGE_BLAST_WAVE,
+    GLYPH_MINOR_MAGE_FROST_WARD,
+    GLYPH_MINOR_MAGE_FIRE_WARD,
+    GLYPH_MINOR_MAGE_SLOW_FALL,
+    GLYPH_MINOR_MAGE_THE_PENGUIN,
+    GLYPH_MINOR_MAGE_ARCANE_INTELLECT,
+    GLYPH_MINOR_MAGE_FROST_ARMOR,
+]
