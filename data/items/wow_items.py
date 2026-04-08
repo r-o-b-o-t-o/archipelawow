cc31471c7993c651b1e99c36383f5ffddb5f098b
Axel Cocat <ax.cocat@gmail.com>
2026-04-08 23:30:46 +0200

    feat(items/wow_items): add warlock glyphs

--- data/items/wow_items.py
+++ data/items/wow_items.py
@@ -556,3 +556,76 @@ GLYPHS_MAGE = [
     GLYPH_MINOR_MAGE_ARCANE_INTELLECT,
     GLYPH_MINOR_MAGE_FROST_ARMOR,
 ]
+
+GLYPH_MAJOR_WARLOCK_UNSTABLE_AFFLICTION = WoWItem("Glyph of Unstable Affliction", 42472, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_FELGUARD = WoWItem("Glyph of Felguard", 42459, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_DEATH_COIL = WoWItem("Glyph of Death Coil", 42457, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_HOWL_OF_TERROR = WoWItem("Glyph of Howl of Terror", 42463, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_HAUNT = WoWItem("Glyph of Haunt", 45779, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_METAMORPHOSIS = WoWItem("Glyph of Metamorphosis", 45780, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_CHAOS_BOLT = WoWItem("Glyph of Chaos Bolt", 45781, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_DEMONIC_CIRCLE = WoWItem("Glyph of Demonic Circle", 45782, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SHADOWFLAME = WoWItem("Glyph of Shadowflame", 45783, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_LIFE_TAP = WoWItem("Glyph of Life Tap", 45785, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SOUL_LINK = WoWItem("Glyph of Soul Link", 45789, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_CONFLAGRATE = WoWItem("Glyph of Conflagrate", 42454, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_FELHUNTER = WoWItem("Glyph of Felhunter", 42460, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SIPHON_LIFE = WoWItem("Glyph of Siphon Life", 42469, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_INCINERATE = WoWItem("Glyph of Incinerate", 42453, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SUCCUBUS = WoWItem("Glyph of Succubus", 42471, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SHADOWBURN = WoWItem("Glyph of Shadowburn", 42468, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SEARING_PAIN = WoWItem("Glyph of Searing Pain", 42466, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SOULSTONE = WoWItem("Glyph of Soulstone", 42470, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_HEALTH_FUNNEL = WoWItem("Glyph of Health Funnel", 42461, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_QUICK_DECAY = WoWItem("Glyph of Quick Decay", 50077, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_VOIDWALKER = WoWItem("Glyph of Voidwalker", 42473, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_HEALTHSTONE = WoWItem("Glyph of Healthstone", 42462, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_CORRUPTION = WoWItem("Glyph of Corruption", 42455, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_FEAR = WoWItem("Glyph of Fear", 42458, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_CURSE_OF_AGONY = WoWItem("Glyph of Curse of Agony", 42456, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_IMP = WoWItem("Glyph of Imp", 42465, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_SHADOW_BOLT = WoWItem("Glyph of Shadow Bolt", 42467, ItemClassification.useful)
+GLYPH_MAJOR_WARLOCK_IMMOLATE = WoWItem("Glyph of Immolate", 42464, ItemClassification.useful)
+GLYPH_MINOR_WARLOCK_CURSE_OF_EXHAUSTION = WoWItem("Glyph of Curse of Exhaustion", 43392, ItemClassification.filler)
+GLYPH_MINOR_WARLOCK_SOULS = WoWItem("Glyph of Souls", 43394, ItemClassification.filler)
+GLYPH_MINOR_WARLOCK_ENSLAVE_DEMON = WoWItem("Glyph of Enslave Demon", 43393, ItemClassification.filler)
+GLYPH_MINOR_WARLOCK_KILROGG = WoWItem("Glyph of Kilrogg", 43391, ItemClassification.filler)
+GLYPH_MINOR_WARLOCK_DRAIN_SOUL = WoWItem("Glyph of Drain Soul", 43390, ItemClassification.filler)
+GLYPH_MINOR_WARLOCK_UNENDING_BREATH = WoWItem("Glyph of Unending Breath", 43389, ItemClassification.filler)
+GLYPHS_WARLOCK = [
+    GLYPH_MAJOR_WARLOCK_UNSTABLE_AFFLICTION,
+    GLYPH_MAJOR_WARLOCK_FELGUARD,
+    GLYPH_MAJOR_WARLOCK_DEATH_COIL,
+    GLYPH_MAJOR_WARLOCK_HOWL_OF_TERROR,
+    GLYPH_MAJOR_WARLOCK_HAUNT,
+    GLYPH_MAJOR_WARLOCK_METAMORPHOSIS,
+    GLYPH_MAJOR_WARLOCK_CHAOS_BOLT,
+    GLYPH_MAJOR_WARLOCK_DEMONIC_CIRCLE,
+    GLYPH_MAJOR_WARLOCK_SHADOWFLAME,
+    GLYPH_MAJOR_WARLOCK_LIFE_TAP,
+    GLYPH_MAJOR_WARLOCK_SOUL_LINK,
+    GLYPH_MAJOR_WARLOCK_CONFLAGRATE,
+    GLYPH_MAJOR_WARLOCK_FELHUNTER,
+    GLYPH_MAJOR_WARLOCK_SIPHON_LIFE,
+    GLYPH_MAJOR_WARLOCK_INCINERATE,
+    GLYPH_MAJOR_WARLOCK_SUCCUBUS,
+    GLYPH_MAJOR_WARLOCK_SHADOWBURN,
+    GLYPH_MAJOR_WARLOCK_SEARING_PAIN,
+    GLYPH_MAJOR_WARLOCK_SOULSTONE,
+    GLYPH_MAJOR_WARLOCK_HEALTH_FUNNEL,
+    GLYPH_MAJOR_WARLOCK_QUICK_DECAY,
+    GLYPH_MAJOR_WARLOCK_VOIDWALKER,
+    GLYPH_MAJOR_WARLOCK_HEALTHSTONE,
+    GLYPH_MAJOR_WARLOCK_CORRUPTION,
+    GLYPH_MAJOR_WARLOCK_FEAR,
+    GLYPH_MAJOR_WARLOCK_CURSE_OF_AGONY,
+    GLYPH_MAJOR_WARLOCK_IMP,
+    GLYPH_MAJOR_WARLOCK_SHADOW_BOLT,
+    GLYPH_MAJOR_WARLOCK_IMMOLATE,
+    GLYPH_MINOR_WARLOCK_CURSE_OF_EXHAUSTION,
+    GLYPH_MINOR_WARLOCK_SOULS,
+    GLYPH_MINOR_WARLOCK_ENSLAVE_DEMON,
+    GLYPH_MINOR_WARLOCK_KILROGG,
+    GLYPH_MINOR_WARLOCK_DRAIN_SOUL,
+    GLYPH_MINOR_WARLOCK_UNENDING_BREATH,
+]
