4af0749365e75476e4bce0def8ad5b256f4b5cd5
Axel Cocat <ax.cocat@gmail.com>
2026-04-08 23:13:07 +0200

    feat(items/wow_items): add druid glyphs

--- data/items/wow_items.py
+++ data/items/wow_items.py
@@ -402,3 +402,82 @@ GLYPHS_ROGUE = [
     GLYPH_MINOR_ROGUE_BLURRED_SPEED,
     GLYPH_MINOR_ROGUE_PICK_POCKET,
 ]
+
+GLYPH_MAJOR_DRUID_LIFEBLOOM = WoWItem("Glyph of Lifebloom", 40915, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_FOCUS = WoWItem("Glyph of Focus", 44928, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_STARFALL = WoWItem("Glyph of Starfall", 40921, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_MANGLE = WoWItem("Glyph of Mangle", 40900, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_SAVAGE_ROAR = WoWItem("Glyph of Savage Roar", 45604, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_NOURISH = WoWItem("Glyph of Nourish", 45603, ItemClassification.useful)
+# GLYPH_MAJOR_DRUID_WILD_GROWTH = WoWItem("Glyph of Wild Growth", 45602, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_BERSERK = WoWItem("Glyph of Berserk", 45601, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_MONSOON = WoWItem("Glyph of Monsoon", 45622, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_BARKSKIN = WoWItem("Glyph of Barkskin", 45623, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_HURRICANE = WoWItem("Glyph of Hurricane", 40920, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_INNERVATE = WoWItem("Glyph of Innervate", 40908, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_SWIFTMEND = WoWItem("Glyph of Swiftmend", 40906, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_FRENZIED_REGENERATION = WoWItem("Glyph of Frenzied Regeneration", 40896, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_RAKE = WoWItem("Glyph of Rake", 40903, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_SHRED = WoWItem("Glyph of Shred", 40901, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_STARFIRE = WoWItem("Glyph of Starfire", 40916, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_INSECT_SWARM = WoWItem("Glyph of Insect Swarm", 40919, ItemClassification.useful)
+# GLYPH_MAJOR_DRUID_REBIRTH = WoWItem("Glyph of Rebirth", 40909, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_RIP = WoWItem("Glyph of Rip", 40902, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_CLAW = WoWItem("Glyph of Claw", 48720, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_SURVIVAL_INSTINCTS = WoWItem("Glyph of Survival Instincts", 46372, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_REGROWTH = WoWItem("Glyph of Regrowth", 40912, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_MOONFIRE = WoWItem("Glyph of Moonfire", 40923, ItemClassification.useful)
+# GLYPH_MAJOR_DRUID_GROWL = WoWItem("Glyph of Growl", 40899, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_MAUL = WoWItem("Glyph of Maul", 40897, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_ENTANGLING_ROOTS = WoWItem("Glyph of Entangling Roots", 40924, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_HEALING_TOUCH = WoWItem("Glyph of Healing Touch", 40914, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_REJUVENATION = WoWItem("Glyph of Rejuvenation", 40913, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_RAPID_REJUVENATION = WoWItem("Glyph of Rapid Rejuvenation", 50125, ItemClassification.useful)
+GLYPH_MAJOR_DRUID_WRATH = WoWItem("Glyph of Wrath", 40922, ItemClassification.useful)
+GLYPH_MINOR_DRUID_TYPHOON = WoWItem("Glyph of Typhoon", 44922, ItemClassification.filler)
+GLYPH_MINOR_DRUID_CHALLENGING_ROAR = WoWItem("Glyph of Challenging Roar", 43334, ItemClassification.filler)
+GLYPH_MINOR_DRUID_UNBURDENED_REBIRTH = WoWItem("Glyph of Unburdened Rebirth", 43331, ItemClassification.filler)
+GLYPH_MINOR_DRUID_AQUATIC_FORM = WoWItem("Glyph of Aquatic Form", 43316, ItemClassification.filler)
+GLYPH_MINOR_DRUID_DASH = WoWItem("Glyph of Dash", 43674, ItemClassification.filler)
+GLYPH_MINOR_DRUID_THORNS = WoWItem("Glyph of Thorns", 43332, ItemClassification.filler)
+GLYPH_MINOR_DRUID_THE_WILD = WoWItem("Glyph of the Wild", 43335, ItemClassification.filler)
+GLYPHS_DRUID = [
+    GLYPH_MAJOR_DRUID_LIFEBLOOM,
+    GLYPH_MAJOR_DRUID_FOCUS,
+    GLYPH_MAJOR_DRUID_STARFALL,
+    GLYPH_MAJOR_DRUID_MANGLE,
+    GLYPH_MAJOR_DRUID_SAVAGE_ROAR,
+    GLYPH_MAJOR_DRUID_NOURISH,
+    # GLYPH_MAJOR_DRUID_WILD_GROWTH,
+    GLYPH_MAJOR_DRUID_BERSERK,
+    GLYPH_MAJOR_DRUID_MONSOON,
+    GLYPH_MAJOR_DRUID_BARKSKIN,
+    GLYPH_MAJOR_DRUID_HURRICANE,
+    GLYPH_MAJOR_DRUID_INNERVATE,
+    GLYPH_MAJOR_DRUID_SWIFTMEND,
+    GLYPH_MAJOR_DRUID_FRENZIED_REGENERATION,
+    GLYPH_MAJOR_DRUID_RAKE,
+    GLYPH_MAJOR_DRUID_SHRED,
+    GLYPH_MAJOR_DRUID_STARFIRE,
+    GLYPH_MAJOR_DRUID_INSECT_SWARM,
+    # GLYPH_MAJOR_DRUID_REBIRTH,
+    GLYPH_MAJOR_DRUID_RIP,
+    GLYPH_MAJOR_DRUID_CLAW,
+    GLYPH_MAJOR_DRUID_SURVIVAL_INSTINCTS,
+    GLYPH_MAJOR_DRUID_REGROWTH,
+    GLYPH_MAJOR_DRUID_MOONFIRE,
+    # GLYPH_MAJOR_DRUID_GROWL,
+    GLYPH_MAJOR_DRUID_MAUL,
+    GLYPH_MAJOR_DRUID_ENTANGLING_ROOTS,
+    GLYPH_MAJOR_DRUID_HEALING_TOUCH,
+    GLYPH_MAJOR_DRUID_REJUVENATION,
+    GLYPH_MAJOR_DRUID_RAPID_REJUVENATION,
+    GLYPH_MAJOR_DRUID_WRATH,
+    GLYPH_MINOR_DRUID_TYPHOON,
+    GLYPH_MINOR_DRUID_CHALLENGING_ROAR,
+    GLYPH_MINOR_DRUID_UNBURDENED_REBIRTH,
+    GLYPH_MINOR_DRUID_AQUATIC_FORM,
+    GLYPH_MINOR_DRUID_DASH,
+    GLYPH_MINOR_DRUID_THORNS,
+    GLYPH_MINOR_DRUID_THE_WILD,
+]
