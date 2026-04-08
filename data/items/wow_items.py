7db1c488f1fb63e4ec09c253af5ab1bdabf810ae
Axel Cocat <ax.cocat@gmail.com>
2026-04-08 23:43:25 +0200

    feat(items/wow_items): add priest glyphs

--- data/items/wow_items.py
+++ data/items/wow_items.py
@@ -629,3 +629,72 @@ GLYPHS_WARLOCK = [
     GLYPH_MINOR_WARLOCK_DRAIN_SOUL,
     GLYPH_MINOR_WARLOCK_UNENDING_BREATH,
 ]
+
+# GLYPH_MAJOR_PRIEST_MASS_DISPEL = WoWItem("Glyph of Mass Dispel", 42404, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_SHADOW_WORD_DEATH = WoWItem("Glyph of Shadow Word: Death", 42414, ItemClassification.useful)
+# GLYPH_MAJOR_PRIEST_CIRCLE_OF_HEALING = WoWItem("Glyph of Circle of Healing", 42396, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_PAIN_SUPPRESSION = WoWItem("Glyph of Pain Suppression", 45760, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_HYMN_OF_HOPE = WoWItem("Glyph of Hymn of Hope", 45758, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_MIND_SEAR = WoWItem("Glyph of Mind Sear", 45757, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_PENANCE = WoWItem("Glyph of Penance", 45756, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_GUARDIAN_SPIRIT = WoWItem("Glyph of Guardian Spirit", 45755, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_DISPERSION = WoWItem("Glyph of Dispersion", 45753, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_LIGHTWELL = WoWItem("Glyph of Lightwell", 42403, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_PRAYER_OF_HEALING = WoWItem("Glyph of Prayer of Healing", 42409, ItemClassification.useful)
+# GLYPH_MAJOR_PRIEST_SPIRIT_OF_REDEMPTION = WoWItem("Glyph of Spirit of Redemption", 42417, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_MIND_CONTROL = WoWItem("Glyph of Mind Control", 42405, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_SHADOW = WoWItem("Glyph of Shadow", 42407, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_SCOURGE_IMPRISONMENT = WoWItem("Glyph of Scourge Imprisonment", 42412, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_HOLY_NOVA = WoWItem("Glyph of Holy Nova", 42401, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_FLASH_HEAL = WoWItem("Glyph of Flash Heal", 42400, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_FEAR_WARD = WoWItem("Glyph of Fear Ward", 42399, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_DISPEL_MAGIC = WoWItem("Glyph of Dispel Magic", 42397, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_MIND_FLAY = WoWItem("Glyph of Mind Flay", 42415, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_PSYCHIC_SCREAM = WoWItem("Glyph of Psychic Scream", 42410, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_INNER_FIRE = WoWItem("Glyph of Inner Fire", 42402, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_SHADOW_WORD_PAIN = WoWItem("Glyph of Shadow Word: Pain", 42406, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_RENEW = WoWItem("Glyph of Renew", 42411, ItemClassification.useful)
+# GLYPH_MAJOR_PRIEST_FADE = WoWItem("Glyph of Fade", 42398, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_POWER_WORD_SHIELD = WoWItem("Glyph of Power Word: Shield", 42408, ItemClassification.useful)
+GLYPH_MAJOR_PRIEST_SMITE = WoWItem("Glyph of Smite", 42416, ItemClassification.useful)
+GLYPH_MINOR_PRIEST_SHADOWFIEND = WoWItem("Glyph of Shadowfiend", 43374, ItemClassification.filler)
+GLYPH_MINOR_PRIEST_LEVITATE = WoWItem("Glyph of Levitate", 43370, ItemClassification.filler)
+GLYPH_MINOR_PRIEST_SHADOW_PROTECTION = WoWItem("Glyph of Shadow Protection", 43372, ItemClassification.filler)
+GLYPH_MINOR_PRIEST_SHACKLE_UNDEAD = WoWItem("Glyph of Shackle Undead", 43373, ItemClassification.filler)
+# GLYPH_MINOR_PRIEST_FADING = WoWItem("Glyph of Fading", 43342, ItemClassification.filler)
+GLYPH_MINOR_PRIEST_FORTITUDE = WoWItem("Glyph of Fortitude", 43371, ItemClassification.filler)
+GLYPHS_PRIEST = [
+    # GLYPH_MAJOR_PRIEST_MASS_DISPEL,
+    GLYPH_MAJOR_PRIEST_SHADOW_WORD_DEATH,
+    # GLYPH_MAJOR_PRIEST_CIRCLE_OF_HEALING,
+    GLYPH_MAJOR_PRIEST_PAIN_SUPPRESSION,
+    GLYPH_MAJOR_PRIEST_HYMN_OF_HOPE,
+    GLYPH_MAJOR_PRIEST_MIND_SEAR,
+    GLYPH_MAJOR_PRIEST_PENANCE,
+    GLYPH_MAJOR_PRIEST_GUARDIAN_SPIRIT,
+    GLYPH_MAJOR_PRIEST_DISPERSION,
+    GLYPH_MAJOR_PRIEST_LIGHTWELL,
+    GLYPH_MAJOR_PRIEST_PRAYER_OF_HEALING,
+    # GLYPH_MAJOR_PRIEST_SPIRIT_OF_REDEMPTION,
+    GLYPH_MAJOR_PRIEST_MIND_CONTROL,
+    GLYPH_MAJOR_PRIEST_SHADOW,
+    GLYPH_MAJOR_PRIEST_SCOURGE_IMPRISONMENT,
+    GLYPH_MAJOR_PRIEST_HOLY_NOVA,
+    GLYPH_MAJOR_PRIEST_FLASH_HEAL,
+    GLYPH_MAJOR_PRIEST_FEAR_WARD,
+    GLYPH_MAJOR_PRIEST_DISPEL_MAGIC,
+    GLYPH_MAJOR_PRIEST_MIND_FLAY,
+    GLYPH_MAJOR_PRIEST_PSYCHIC_SCREAM,
+    GLYPH_MAJOR_PRIEST_INNER_FIRE,
+    GLYPH_MAJOR_PRIEST_SHADOW_WORD_PAIN,
+    GLYPH_MAJOR_PRIEST_RENEW,
+    # GLYPH_MAJOR_PRIEST_FADE,
+    GLYPH_MAJOR_PRIEST_POWER_WORD_SHIELD,
+    GLYPH_MAJOR_PRIEST_SMITE,
+    GLYPH_MINOR_PRIEST_SHADOWFIEND,
+    GLYPH_MINOR_PRIEST_LEVITATE,
+    GLYPH_MINOR_PRIEST_SHADOW_PROTECTION,
+    GLYPH_MINOR_PRIEST_SHACKLE_UNDEAD,
+    # GLYPH_MINOR_PRIEST_FADING,
+    GLYPH_MINOR_PRIEST_FORTITUDE,
+]
